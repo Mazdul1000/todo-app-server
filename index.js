75fcd821d04080f3c5197ef6447f0d768a086cdd
Mazedul Hasan <mazdul1000@gmail.com>
2022-08-27 01:55:30 +0600

    setup the database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion} = require('mongodb');
 const query = require('express/lib/middleware/query');
 require('dotenv').config();
 const app = express();
@@ -16,8 +16,42 @@ app.use(express.json());
 
 
 // Connect with mongoDB
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@todo-app.kya6j6y.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.strweqb.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+
+        await client.connect();
+
+        const userCollection = client.db('todo-db').collection('users');
+        // const taskCollection = client.db('Todo').collection('tasks');
+
+        const user = {name: "Mazdul", email: "mazdddul@mail.com"}
+        const result = await userCollection.insertOne(user);
+
+
+
+        // Get all the task 
+        app.get('/movies', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const movies = await cursor.toArray();
+            res.send(movies);
+      
+        });
+        
+    }
+
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Running Todo app server')
