24e065e1b36fdba71a92706db9d1afb3aa805f46
Mazedul Hasan <mazdul1000@gmail.com>
2022-08-26 23:58:08 +0600

    setup jwt

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-// const jwt = require('jsonwebtoken');
-// const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
-// const query = require('express/lib/middleware/query');
-// require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+const query = require('express/lib/middleware/query');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5001;
 
@@ -15,7 +15,9 @@ app.use(express.json());
 
 
 
-
+// Connect with mongoDB
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@todo-app.kya6j6y.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.get('/', (req, res) => {
   res.send('Running Todo app server')
